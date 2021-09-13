f05f1f837a882e198ad89cf56829a399aa2958db
S S MERCY QUEEN <55204010+MercyQueen@users.noreply.github.com>
2021-09-13 21:30:15 +0530

    Add files via upload

---
+++ src/javaapplication10/Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author User
+ */
+public class Node {
+    int key;
+    Node left;
+    Node right;
+    Node(int k)
+    {
+        key=k;
+    }
+}
